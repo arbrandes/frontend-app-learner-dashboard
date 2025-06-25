30efdabddf5fb89443dab6607648d38eface0691
Adolfo R. Brandes <adolfo@axim.org>
2025-06-25 11:38:27 -0300

    [WIP] fix tests

--- src/hooks/api.test.js
+++ src/hooks/api.test.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { SiteContext } from '@openedx/frontend-base';
-import { keyStore } from 'utils';
-import { RequestKeys } from 'data/constants/requests';
-import { post } from 'data/services/lms/utils';
-import api from 'data/services/lms/api';
+import { keyStore } from '../utils';
+import { RequestKeys } from '../data/constants/requests';
+import { post } from '../data/services/lms/utils';
+import api from '../data/services/lms/api';
 
-import * as reduxHooks from 'data/redux/hooks';
+import * as reduxHooks from '../data/redux/hooks';
 import * as apiHooks from './api';
 
 const reduxKeys = keyStore(reduxHooks);
 
-jest.mock('data/services/lms/utils', () => ({
+jest.mock('@tanstack/react-query', () => ({
+  ...jest.requireActual('@tanstack/react-query'),
+  useQuery: jest.fn(),
+}));
+
+jest.mock('../data/services/lms/utils', () => ({
   post: jest.fn((...args) => ({ post: args })),
 }));
-jest.mock('data/services/lms/api', () => ({
+jest.mock('../data/services/lms/api', () => ({
   initializeList: jest.fn(),
   updateEntitlementEnrollment: jest.fn(),
   unenrollFromCourse: jest.fn(),
@@ -21,7 +26,7 @@ jest.mock('data/services/lms/api', () => ({
   updateEmailSettings: jest.fn(),
   createCreditRequest: jest.fn(),
 }));
-jest.mock('data/redux/hooks', () => ({
+jest.mock('../data/redux/hooks', () => ({
   useCardCourseRunData: jest.fn(),
   useCardCreditData: jest.fn(),
   useCardEntitlementData: jest.fn(),
